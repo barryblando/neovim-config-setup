acf611a11d916e784b8ba543f496777cb07382cf
barryblando <barryblando@gmail.com>
2022-05-21 18:13:14 +0800

    use vscode theme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,10 @@
+vim.g.vscode_style = "dark"
+vim.g.vscode_transparent = 1
+vim.g.vscode_disable_nvimtree_bg = true
+
 vim.cmd [[
 try
-  colorscheme darkplus
+  colorscheme vscode 
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
