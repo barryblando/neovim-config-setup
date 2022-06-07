735c78d70a72dc2158febe9e60b071043e4a9da5
barryblando <barryblando@gmail.com>
2022-06-07 08:36:26 +0800

    use gruvbox-material as default colorscheme to match wezterm.

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,15 +1,40 @@
-vim.opt.termguicolors = true
-vim.g.vscode_style = "dark"
-vim.g.vscode_transparent = 1
-vim.g.vscode_disable_nvimtree_bg = true
+-- vim.opt.termguicolors = true
+-- vim.g.vscode_style = "dark"
+-- vim.g.vscode_transparent = 1
+-- vim.g.vscode_disable_nvimtree_bg = true
+
+-- vim.cmd [[
+-- try
+--   colorscheme vscode 
+-- catch /^Vim\%((\a\+)\)\=:E185/
+--   colorscheme default
+--   set background=dark
+-- endtry
+-- ]]
 
 vim.cmd [[
-try
-  colorscheme vscode 
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
+  " Important!! https://github.com/sainnhe/gruvbox-material/blob/master/doc/gruvbox-material.txt
+  if has('termguicolors')
+    set termguicolors
+  endif
+
+  " For dark version.
   set background=dark
-endtry
+   
+  " This configuration option should be placed before `colorscheme gruvbox-material`.
+  " Available values: 'hard', 'medium'(default), 'soft'
+  let g:gruvbox_material_background = 'hard'
+
+  let g:gruvbox_material_foreground = 'mix'
+
+  let g:gruvbox_material_transparent_background = 1
+  
+  " For better performance
+  let g:gruvbox_material_better_performance = 1
+
+  let g:gruvbox_material_enable_italic = 1
+  
+  colorscheme gruvbox-material
 ]]
 
 -- Set bufferline bg transparent
