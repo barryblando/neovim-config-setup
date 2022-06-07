20bf2cadff757ce63c6278bb29a8751dbc0e4462
barryblando <barryblando@gmail.com>
2022-06-07 08:33:53 +0800

    add specifc json syntax for tsconfig: JSONC. To supress unwanted error.

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -19,6 +19,12 @@ vim.cmd [[
     autocmd FileType markdown setlocal spell
   augroup end
 
+  augroup _json
+    autocmd!
+    autocmd BufNewFile,BufRead tsconfig.json setlocal filetype=jsonc
+    autocmd BufNewFile,BufRead tsconfig.*.json setlocal filetype=jsonc
+  augroup end
+  
   augroup _auto_resize
     autocmd!
     autocmd VimResized * tabdo wincmd = 
