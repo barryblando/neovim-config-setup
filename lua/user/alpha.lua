07930ac06e19f0e6a85985e23cca8d15ee054df6
barryblando <barryblando@gmail.com>
2022-06-12 20:12:40 +0800

    change link.

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -30,7 +30,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	return "https://github.com/barryblando"
 end
 
 dashboard.section.footer.val = footer()
