3cc8b56b9d16a81652ae1fa78ebdc862e22a7f8e
barryblando <barryblando@gmail.com>
2022-06-13 20:48:11 +0800

    setup lsp signature.

---
+++ lua/lsp/signature.lua
@@ -0,0 +1,20 @@
+local ok_status, signature = pcall(require, "lsp_signature")
+
+if not ok_status then
+  return
+end
+
+signature.setup {
+  -- general options
+    -- general options
+  always_trigger = "true",
+  hint_enable = false, -- virtual text hint
+
+  -- floating window
+  padding = " ",
+  transparency = nil,
+  handler_opts = {
+    border = { "┏", "━", "┓", "┃", "┛","━", "┗", "┃" },
+  },
+}
+
