c217040ac723bb65a80ae338f9872d92df5ae400
barryblando <barryblando@gmail.com>
2022-06-13 21:17:12 +0800

    add fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -123,6 +123,13 @@ telescope.setup {
         },
       },
     },
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    },
     -- ["ui-select"] = {
     --   require("telescope.themes").get_dropdown {
     --     previewer = false,
@@ -134,3 +141,4 @@ telescope.setup {
 
 -- telescope.load_extension "ui-select"
 telescope.load_extension "file_browser"
+telescope.load_extension "fzf"
