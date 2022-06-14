1a1d12905ca90d6c37ee69ea62c84d9ae1c5b73a
barryblando <barryblando@gmail.com>
2022-06-14 15:20:21 +0800

    disable nvim-tree for Neotree. Just enable file explorer you want to use.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -81,7 +81,43 @@ return packer.startup(function(use)
   use { "numToStr/Comment.nvim", config = lua_path"comment" }
 
   -- File Explorer
-  use { "kyazdani42/nvim-tree.lua", config = lua_path"nvim-tree" }
+  use ({
+    "kyazdani42/nvim-tree.lua",
+    disable = true,
+    config = lua_path"nvim-tree"
+  })
+
+  use({
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      {
+        -- only needed if you want to use the commands with "_with_window_picker" suffix
+        's1n7ax/nvim-window-picker',
+        tag = "1.*",
+        config = function()
+          require'window-picker'.setup({
+            autoselect_one = true,
+            include_current = false,
+            filter_rules = {
+              -- filter using buffer options
+              bo = {
+                -- if the file type is one of following, the window will be ignored
+                filetype = { 'neo-tree', "neo-tree-popup", "notify", "quickfix" },
+
+                -- if the buffer type is one of following, the window will be ignored
+                buftype = { 'terminal' },
+              },
+            },
+            other_win_hl_color = '#e35e4f',
+          })
+        end,
+      }
+    },
+    config = lua_path"nvim-neo-tree",
+  })
 
   -- Tabs
   use { "akinsho/bufferline.nvim", config = lua_path"bufferline" }
