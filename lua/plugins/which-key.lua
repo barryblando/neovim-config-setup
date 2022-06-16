05d4d04dab0d419d4436e9ef712324f9dda6bc30
barryblando <barryblando@gmail.com>
2022-06-16 21:15:56 +0800

    change code action menu

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -151,7 +151,7 @@ local mappings = {
 
   l = {
     name = "LSP",
-    a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+    a = { "<cmd>CodeActionMenu<cr>", "Code Action" },
     d = {
       "<cmd>Telescope lsp_document_diagnostics<cr>",
       "Document Diagnostics",
