08ec2ac34ad0f463a308f8441843fbc3558f3c0b
barryblando <barryblando@gmail.com>
2022-06-18 22:31:35 +0800

    format by null-ls

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,10 +1,10 @@
 local ok_status, colorizer = pcall(require, "colorizer")
 
 if not ok_status then
-  return
+	return
 end
 
-colorizer.setup {
-  -- Highlight all files
-  '*';
-}
+colorizer.setup({
+	-- Highlight all files
+	"*",
+})
