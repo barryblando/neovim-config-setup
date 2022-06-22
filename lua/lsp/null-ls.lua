53dbb62d542f8cc031d07aca90103a05ec5635ac
barryblando <barryblando@gmail.com>
2022-06-22 17:50:13 +0800

    update null-ls.

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -11,7 +11,53 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		-- Basic Formatting needs
+		formatting.prettier.with({
+			filetypes = {
+				"css",
+				"scss",
+				"less",
+				"html",
+				"json",
+				"jsonc",
+				"yaml",
+				"markdown",
+				"graphql",
+				"handlebars",
+			},
+			condition = function(utils)
+				return utils.root_has_file({
+					".prettierrc.json",
+					".prettierignore",
+					".prettierrc.yml",
+					".prettierrc.yaml",
+					".prettierrc.json5",
+					".prettierrc.js",
+					".prettierrc.cjs",
+					".prettierrc.config.js",
+					".prettierrc.config.cjs",
+				})
+			end,
+			prefer_local = "node_modules/.bin", -- find local prettier, fall back to global prettier if it can't find one locally
+		}),
+
+		-- Typescript, Javascript, React, Vue
+		formatting.eslint.with({}),
+		diagnostics.eslint.with({}),
+
+		-- Lua
 		formatting.stylua,
+
+		-- Docker (brew install hadolint, scoop install hadolint)
+		diagnostics.hadolint,
+
+		-- Protocol Buffers
+		formatting.buf,
+		diagnostics.buf,
+
+		-- Ansible
+		diagnostics.ansiblelint,
+
+		-- null_ls.builtins.code_actions.refactoring,
 	},
 })
